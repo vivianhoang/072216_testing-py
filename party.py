5e2ae5f8e45c69463e6d708b22f0a690f322b67d
Hackbright Student <info@hackbrightacademy.com>
2016-07-22 12:20:37 -0700

    Test if RSVP is Mel or his email

--- party.py
+++ party.py
@@ -9,7 +9,21 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+
+    >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+    True
+
+    >>> is_mel('Judith Butler', 'judith@awesome.com')
+    False
+
+    >>> is_mel('Mel Melitpolski', '')
+    True
+
+    >>> is_mel('', 'mel@ubermelon.com')
+    True
+
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
